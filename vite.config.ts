bf54526f05c38eb2731918c00db5edbf4a29b21a
Calvin Sass <calvinsass@outlook.com>
2025-05-10 15:20:13 +0200

    Change the ports in vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+      "/api": {
+        target: "https://task-app-backend-r2t0.onrender.com/",
         changeOrigin: true,
-      }
-    }
-  }
-})
-
+      },
+    },
+  },
+});
