3684e4d8aa2d76e2b5fca779ee1b43bb8ee1d524
Calvin Sass <calvinsass@outlook.com>
2025-05-11 12:13:21 +0200

    Fixed typo

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -177,7 +177,7 @@ export const AppProvider: React.FC<{ children: React.ReactNode }> = ({
 
   const API_BASE_URL =
     import.meta.env.VITE_API_BASE_URL ||
-    "https://task-app-backend-r2t0.onrender.com/api";
+    "https://task-app-backend-r2t0.onrender.com";
 
   // API calls
   const api: AppContextType["api"] = {
